d7fae85d359d04bbb122989fb46e51650b484183
Haaris Khatri <mahamadhariskhatri19@gnu.ac.in>
2021-07-05 15:30:02 +0530

    made employee form as index page for the project

--- crud/employee_register/urls.py
+++ crud/employee_register/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form), 
-    path('list/', views.employee_list)
-    
+    path('', views.employee_form,name= 'employee_insert'), #get and post request for insert
+    path('<int:id>/', views.employee_form,name='employee_update'), #get and post request for update 
+    path('list/', views.employee_list,name = 'employee_list'), #get request to fetch all records
+    path('delete/<int:id>/', views.employee_delete,name='employee_delete'),
 ]
