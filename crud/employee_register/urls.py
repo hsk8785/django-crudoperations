2aedec9414b137a1ac73cc24cade243e8b64afc4
Haaris Khatri <mahamadhariskhatri19@gnu.ac.in>
2021-07-05 15:55:12 +0530

    Corrected urls.py

--- crud/employee_register/urls.py
+++ crud/employee_register/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form,name= 'employee_insert'), #get and post request for insert
+    path('employee/', views.employee_form,name= 'employee_insert'), #get and post request for insert
     path('<int:id>/', views.employee_form,name='employee_update'), #get and post request for update 
     path('list/', views.employee_list,name = 'employee_list'), #get request to fetch all records
     path('delete/<int:id>/', views.employee_delete,name='employee_delete'),
